8afd7fad4e2ca8cda3b0bc47ca6f1d13693c8f56
Eldariur <35937370+Eldariur@users.noreply.github.com>
2018-02-21 12:31:25 +0100

    Need corriger probleme verif email
    POURQUOI CA MARCHE PAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAS

--- actionRegister.php
+++ actionRegister.php
@@ -11,29 +11,51 @@
 	
 	<?php
 	try{
-		$bdd = new PDO('mysql:host=localhost;dbname=l2info;charset=utf8', 'l2infoetu', 'webdyn72');
+		$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
 	}
 	catch (Exception $e){
 		die('Erreur : ' . $e->getMessage());
 	}
+	$email = $_POST['adresse'];
 
-	//Mettre la condition disponibilité e-mail
-
-	$hach_pass = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
-
-	$req = $bdd->prepare('INSERT INTO account(email,pass,fname,name,promotion)VALUES(:email,:pass,:fname,:name,:promotion) ');
-	$req->execute(array(
-		'email' => $_POST['adresse'],
-		'pass' => $hach_pass,
-		'fname' => $_POST['nom'],
-		'name' => $_POST['prenom'],
-		'promotion' => $_POST['promotion']
+	// condition disponibilité e-mail
+	$req2 = $bdd->prepare('SELECT email FROM account WHERE email=:email');
+	$req2->execute(array(
+		'email' => $email
 	));
+	$verif = $req2->fetch();
+	if($email = $verif['email'])
+	{
+	?>	
+	<p>Adresse déjà utilisée</p>
+	<p><a href="REGISTER.php">Réessayez</a> l'inscription</p>
+
+	<?php
+	}
+	else{
+		
+		
+		//Hachage du MDP
+		$hach_pass = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
+		//Entrée des données dans la table
+		$req = $bdd->prepare('INSERT INTO account(email,pass,fname,name,promotion)VALUES(:email,:pass,:fname,:name,:promotion) ');
+		$req->execute(array(
+			'email' => $email,
+			'pass' => $hach_pass,
+			'fname' => $_POST['nom'],
+			'name' => $_POST['prenom'],
+			'promotion' => $_POST['promotion']
+		));
 
 
 	?>
 	
 	<p>Vous vous êtes bien inscrit <?php echo htmlspecialchars($_POST['prenom']); ?> .</p>
+	
+	<?php
+	}
+	?>
+	
 	<p>Cliquez <a href="LOGIN.php">ici</a> pour vous log</p>
 </body>
 
